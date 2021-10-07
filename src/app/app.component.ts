d01cb59c1f5a21764ff16d88eebaff4b8092b9be
thompson <alfredthompsonovie@gmail.com>
2021-10-07 03:22:31 +0100

    build cv with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,27 @@
 import { Component } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStarHalf } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSitemap } from '@fortawesome/free-solid-svg-icons';
+import { faUniversity } from '@fortawesome/free-solid-svg-icons';
+import { faCertificate } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faPalette } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faCss3} from '@fortawesome/free-brands-svg-icons';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
+import { faAndroid } from '@fortawesome/free-brands-svg-icons';
+import { faWordpress } from '@fortawesome/free-brands-svg-icons';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { faSearchengin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +30,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cv';
+  faStar = faStar;
+  faStarHalf = faStarHalf;
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faWordpress = faWordpress;
+  faAndroid = faAndroid;
+  faReact = faReact;
+  faAngular = faAngular;
+  faCss3 = faCss3;
+  faBootstrap = faBootstrap;
+  faJsSquare = faJsSquare;
+  faCss3Alt = faCss3Alt;
+  faHtml5 = faHtml5;
+  faUsers = faUsers;
+  faPalette = faPalette;
+  faPaperPlane = faPaperPlane;
+  faCertificate = faCertificate;
+  faUniversity = faUniversity;
+  faSitemap = faSitemap;
+  faTachometerAlt = faTachometerAlt;
+  faSearchengin = faSearchengin;
+  faImage = faImage;
 }
